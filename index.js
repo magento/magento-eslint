0b3cf76f48238a796c59f3d0a6e4975c4c77f145
zetlen <jzetlen@adobe.com>
2018-11-01 07:42:31 -0500

    feat: add package json linting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = {
-    plugins: ['jsx-a11y', 'react'],
-    extends: ['plugin:jsx-a11y/recommended'],
+    plugins: ['jsx-a11y', 'package-json', 'react'],
+    extends: ['plugin:jsx-a11y/recommended', 'plugin:package-json/recommended'],
     rules: {
         'no-unused-vars': 'error',
         'react/jsx-uses-vars': 'error',
