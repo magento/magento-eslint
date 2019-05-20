c2e5959ffca6979fa0cd8fef2ae78b83e4121f55
Jimmy Sanford <jimbo@users.noreply.github.com>
2019-05-20 14:59:07 -0700

    Add eslint-plugin-react-hooks

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const config = {
-    plugins: ['jsx-a11y', 'package-json', 'react'],
+    plugins: ['jsx-a11y', 'package-json', 'react', 'react-hooks'],
     extends: ['plugin:jsx-a11y/recommended', 'plugin:package-json/recommended'],
     rules: {
         'prefer-const': 'error',
         'no-unused-vars': 'error',
         'react/jsx-uses-vars': 'error',
-        'react/jsx-uses-react': 'error'
+        'react/jsx-uses-react': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error'
     }
 };
 
