20695d40ca70f1cda34900d2516272982828c988
zetlen <jzetlen@adobe.com>
2019-08-15 17:59:57 -0500

    feat: add eslint:recommended and more base config

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const config = {
+    env: {
+        es6: true,
+        jest: true,
+        browser: true,
+        node: true
+    },
     plugins: ['jsx-a11y', 'package-json', 'react', 'react-hooks'],
-    extends: ['plugin:jsx-a11y/recommended', 'plugin:package-json/recommended'],
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'plugin:jsx-a11y/recommended',
+        'plugin:package-json/recommended'
+    ],
     rules: {
+        'getter-return': 'off',
         'prefer-const': 'error',
+        'no-console': 'off',
+        'no-empty': 'off',
         'no-unused-vars': 'error',
         'react/jsx-uses-vars': 'error',
         'react/jsx-uses-react': 'error',
