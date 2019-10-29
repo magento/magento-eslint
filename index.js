46d7e1946bac5e13762c422edc591eaf4b313223
James Zetlen <jzetlen@adobe.com>
2019-10-29 08:30:56 -0500

    feat: add eslint:recommended and more base config (#7)
    * feat: add eslint:recommended and more base config
    
    * fixup PR feedback

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const config = {
+    env: {
+        es6: true
+    },
     plugins: ['jsx-a11y', 'package-json', 'react', 'react-hooks'],
-    extends: ['plugin:jsx-a11y/recommended', 'plugin:package-json/recommended'],
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'plugin:jsx-a11y/recommended',
+        'plugin:package-json/recommended'
+    ],
     rules: {
         'prefer-const': 'error',
+        'no-console': 'off',
         'no-unused-vars': 'error',
         'react/jsx-uses-vars': 'error',
         'react/jsx-uses-react': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/exhaustive-deps': 'error',
         'react-hooks/rules-of-hooks': 'error',
         'no-duplicate-imports': 'error',
         'one-var': ['error', 'never']
