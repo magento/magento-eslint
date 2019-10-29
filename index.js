8d85b7f42d9cca89acc409ca60738e4669bcdb32
Dave Macaulay <daavem@googlemail.com>
2019-10-29 08:27:18 -0500

    Include no-duplicate-imports (#9)
    * Include no-duplicate-imports
    
    * Include one-var 'never'

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const config = {
         'react/jsx-uses-vars': 'error',
         'react/jsx-uses-react': 'error',
         'react-hooks/exhaustive-deps': 'warn',
-        'react-hooks/rules-of-hooks': 'error'
+        'react-hooks/rules-of-hooks': 'error',
+        'no-duplicate-imports': 'error',
+        'one-var': ['error', 'never']
     }
 };
 
